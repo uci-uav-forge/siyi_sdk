7dc90345eed3a8256806ffe910bf6e57acf82d7f
Dat-Bois <eeshvij@gmail.com>
2023-12-02 16:36:57 -0800

    made test script

---
+++ dev/zoom_max.py
@@ -0,0 +1,44 @@
+"""
+@file test_center_gimbal.py
+@Description: This is a test script for using the SIYI SDK Python implementation to adjust zoom level
+@Author: Mohamed Abdelkader
+@Contact: mohamedashraf123@gmail.com
+All rights reserved 2022
+"""
+
+import sys
+import os
+from time import sleep
+from siyi_sdk_src import SIYISDK
+
+def test():
+    cam = SIYISDK(server_ip="192.168.144.25", port=37260)
+
+    if not cam.connect():
+        print("No connection ")
+        exit(1)
+
+    state = True
+    while state:
+        val = input("w to zoom, s to zoom out, q to exit: ")
+        if val == "q":
+            state = False
+        elif val == "w":
+            val = cam.requestZoomIn()
+            sleep(1)
+            val = cam.requestZoomHold()
+            sleep(1)
+            print("Zoom level: ", cam.getZoomLevel())
+        elif val == "s":
+            val = cam.requestZoomOut()
+            sleep(1)
+            val = cam.requestZoomHold()
+            sleep(1)
+            print("Zoom level: ", cam.getZoomLevel())
+        cam.requestAutoFocus()
+        print("Autofocused")
+
+    cam.disconnect()
+
+if __name__ == "__main__":
+    test()
