ecb444c36a9f238e4d9ab1401df7b50fdcdb465c
Dat-Bois <eeshvij@gmail.com>
2023-12-30 12:24:27 -0800

    testing for external abs zoom (works?)

--- dev/zoom_absolute.py
+++ dev/zoom_absolute.py
@@ -1,10 +1,38 @@
 import sys
 import os
-from time import sleep
+from time import sleep, time
 from siyi_sdk import SIYISDK
 
+def manual_absolute_zoom(cam : SIYISDK, target : float):
+    # to get current zoom value
+    cam.requestZoomHold()
+    print("Zoom level: ", cam.getZoomLevel())
+    while cam.getZoomLevel() != target:
+        total_sleep = 0.005
+        if cam.getZoomLevel() > target:
+            cam.requestZoomOut()
+            sleep(0.01)
+            cam.requestZoomHold()
+        elif cam.getZoomLevel() < target:
+            cam.requestZoomIn()
+            sleep(0.01)
+            cam.requestZoomHold()
+        else:
+            cam.requestZoomHold()
+            break
+        if abs(target - cam.getZoomLevel()) < 0.5:
+            total_sleep = 0.05
+        sleep(total_sleep)
+        cam.requestZoomHold()
+        print("Zoom level: ", cam.getZoomLevel())
+    cam.requestZoomHold()
+    sleep(1)
+    print("Zoom level: ", cam.getZoomLevel())
+    cam.requestAutoFocus()
+
+
 def test():
-    cam = SIYISDK(server_ip="192.168.144.25", port=37260, debug=True)
+    cam = SIYISDK(server_ip="192.168.144.25", port=37260, debug=False)
 
     if not cam.connect():
         print("No connection ")
@@ -13,8 +41,10 @@ def test():
     sleep(2)
     #print(cam.requestAbsoluteZoom(4.5))
     cam.requestCenterGimbal()
-    sleep(5)
-    
+    sleep(2)
+    target = 1.5
+    manual_absolute_zoom(cam, target)
+    #manual_absolute_zoom(cam, target)
     #cam.requestFirmwareVersion()
     #sleep(1)
     #print("Camera Firmware version: ", cam.getFirmwareVersion())
