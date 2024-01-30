4808abb46159d3bd0c47f2ae56ac180e8a6d947d
Eric Pedley <ericpedley@gmail.com>
2024-01-30 13:06:37 -0800

    add find_packages to setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(
     name='siyi_sdk_uavf',
     version='0.0.1',
     description='UAV Forge ZR10 Gimbal SDK',
-    author='Eesh Vij and Mohamed Abdelkader'
+    author='Eesh Vij and Mohamed Abdelkader',
+    packages=find_packages(include=['siyi_sdk'])
 )
