a1d33f7fa776b6f5c7afd68e3a9c880d7a725a92
Vince-C156 <vc168888@gmail.com>
2024-04-05 11:29:42 -0700

    add button to record frame

--- dev/stream_test.py
+++ dev/stream_test.py
@@ -17,7 +17,7 @@ def test():
     sleep(2)
     cam.requestLockMode()
     cam.requestCenterGimbal()
-    cam.setAbsoluteZoom(4)
+    cam.setAbsoluteZoom(1)
     sleep(2)
     print("Requesting autofocus in 1 second")
     sleep(1)
@@ -27,9 +27,12 @@ def test():
         frame = stream.get_frame()
         cv2.putText(frame, "Resolution: {}".format(frame.shape), (0, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
         cv2.imshow("Frame", frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        key = cv2.waitKey(1)
+        if key & 0xFF == ord('q'):
             break
-
+        elif key & 0xFF == ord(' '):
+            cv2.imwrite("frame.jpg", frame)
+            
     cam.disconnect()
 
 if __name__ == "__main__":
